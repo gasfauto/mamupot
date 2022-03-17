cae0fe23459c843611d1ab052824808112da079a
Gaspar Heredia <gasfauto@gmail.com>
2022-03-17 06:06:58 -0600

    Se agregó footer

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,13 @@
 const navToggle = document.querySelector(".nav_toggle")
 const navMenu = document.querySelector(".nav_menu")
 
-navToggle.addEventListener("click")
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("nav_menu-visible");
+    
+    if (navMenu.classList.contains("nav_menu-visible")) {
+        navToggle.setAttribute("arial-label", "Cerrar menú");
+    } else {
+        navToggle.setAttribute("arial-label", "Abrir menú");
+    }
+});
+
